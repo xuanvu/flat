3cc6ee58cf9d276a6199c8dc177bcd96421645f5
Vincent Giersch <vincent@giersch.fr>
2012-11-03 16:34:36 +0000

    Fermata.Data: use pseudo-cache for fast access to parts

--- assets/js/Fermata.Data.js
+++ assets/js/Fermata.Data.js
@@ -1,12 +1,55 @@
 var Fermata = Fermata || {};
+ 
+(function () {
+  "use strict";
 
-Fermata.Data = function(score) {
-  this.score = score || {};
-  this.render = []; 
+  Fermata.Data = function (score) {
+    this.score = score || {};
+    this.scoreCache = { parts: null }; // derivated score object for faster access
 
-  console.log(score);
-};
+    // Init new score
+    if (this.score['score-partwise'] === undefined) {
+      this.score['score-partwise'] = {'version': '3.0', 'part-list': {'score-part': null}};
+    }
+  };
 
-Fermata.Data.prototype = {
-  getScore: function() { return this.score; },
-};
+  Fermata.Data.prototype = {
+    // Cache fncs
+    cacheParts: function () {
+      // Only one part, converting into array of objects
+      if (typeof this.score['score-partwise']['part-list']['score-part'] === 'object') {
+        this.score['score-partwise']['part-list']['score-part'] = [this.score['score-partwise']['part-list']['score-part']];
+      }
+
+      if (typeof this.score['score-partwise'].part === 'object') {
+        this.score['score-partwise'].part = [this.score['score-partwise'].part];
+      }
+
+      var cur, cached, i;
+
+      // Index with 'part' idx, ids and names
+      this.scoreCache.parts = { idx: [], id: {}, name: {} };
+      for (i = 0 ; i < this.score['score-partwise']['part-list']['score-part'].length ; ++i) {
+        cur = this.score['score-partwise']['part-list']['score-part'][i];
+        cached = {id: cur.id, name: cur['part-name'].$t, measure: null};
+
+        this.scoreCache.parts.id[cur.id] = cached;
+        this.scoreCache.parts.name[cur['part-name'].$t] = cached;
+        this.scoreCache.parts.idx.push(cached);
+      }
+
+      // Cache fast access to mesures
+      for (i  = 0 ; i < this.score['score-partwise'].part.length ; ++i) {
+        cur = this.score['score-partwise'].part[i];
+        this.scoreCache.parts.id[cur.id].measure = cur.measure;
+      }
+
+      return this.scoreCache.parts;
+    },
+
+    // Getters
+    getScorePartWise: function () { return this.score['score-partwise']; },
+    getParts: function () { return this.scoreCache.parts || this.cacheParts(); }
+  };
+
+}).call(this);
--- assets/js/Fermata.Render.js
+++ assets/js/Fermata.Render.js
@@ -1,18 +1,24 @@
 var Fermata = Fermata || {};
 
-Fermata.Render = function(data, container) {
-    this.data = data;
-    this.container = container;
+(function () {
+  "use strict";
 
-    // Client-side, jQuery selector
-    if (container !== null) {
+  Fermata.Render = function (data, container) {
+      this.data = data;
+      this.container = container;
+
+      // Client-side, jQuery selector
+      if (container !== null) {
         this.container = $(container)[0];
         this.renderer = new Vex.Flow.Renderer(this.container, Vex.Flow.Renderer.Backends.CANVAS);
-    }
-    // Server-side // TODO
-    else {
+      }
+      // Server-side // TODO
+      else {
         this.renderer = new Vex.Flow.Renderer(this.container, Vex.Flow.Renderer.Backends.SVG);
-    }
+      }
 
-    this.ctx = this.renderer.getContext();
-};
+      this.ctx = this.renderer.getContext();
+
+      console.log(data.getParts());
+  };
+}).call(this);
