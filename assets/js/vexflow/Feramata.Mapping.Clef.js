b79bb6723d9e85428db087a133fde7808aa0175b
Cyril Coutelier <cyril.coutelier@epitech.eu>
2012-11-04 03:58:24 +0100

    Ajout d'un fichier de mapping
    Fichier de mapping entre les clef côté
    MusicXML et côté Vexflow.

---
+++ assets/js/vexflow/Feramata.Mapping.Clef.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var Fermata =          Fermata || {};
+Fermata.Mapping =      Fermata.Mapping || {};
+Fermata.Mapping.Clef = Fermata.Mapping.Clef || {};
+
+(function () {
+  "use strict";  
+  
+  var musicXMLToVexflow = {
+    "G": "treble"
+  };
+    
+  var vexFlowToMusicXml = {};
+    
+  for (key in this.musicXMLToVexflow) {
+    var value = musicXMLToVexflow[key];
+      
+    this.vexFlowToMusicXml[value] = key;
+  }
+
+  
+  Fermata.Mapping.Clef.getVexflow = function(musicXml) {
+    return musicXMLToVexflow[musicXml];
+  }
+  
+  Fermata.Mapping.Clef.getMusicXml = function(vexflow) {
+    return vexFlowToMusicXml[vexflow];
+  }
+}).call(this);
