019a4aeccddb602cbb33db5779b28e8ceb79110e
Cyril Coutelier <cyril.coutelier@epitech.eu>
2012-11-04 04:26:29 +0100

    Fonction d'affichage
    Fonction qui commence à afficher la portée
    avec la clef.

--- assets/js/Fermata.Render.js
+++ assets/js/Fermata.Render.js
@@ -22,4 +22,27 @@ var Fermata = Fermata || {};
   //data.sortMeasure();
   //console.log(data.getMesure(1, 'P1'));
   };
+  
+  Fermata.Render.prototype.renderAll = function() {
+    var parts = this.data.getParts();
+      
+    for (var i = 0 ; i < parts.idx.length ; i++) {
+      var part = parts.idx[i];
+              
+      this.render(part.measure[0]);
+    }
+  }
+  
+  
+  Fermata.Render.prototype.render = function (measure) {
+      var stave = new Vex.Flow.Stave(10, 0, 500);
+      var clef = measure["attributes"].clef.sign.$t;
+      var clefName = Fermata.Mapping.Clef.getVexflow(clef);
+      stave.addClef(clefName);
+      stave.setContext(this.ctx);
+      stave.draw();
+      
+      //TODO: to be continued...
+  }
+  
 }).call(this);
