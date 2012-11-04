2e8276c41875d2b54ee0df92453cd614275cd656
Cyril Coutelier <cyril.coutelier@epitech.eu>
2012-11-04 03:33:19 +0100

    Formattage du fichier
    Le fichier a été formatté par  NetBeans avec
    une indentation de 2 espaces.

--- assets/js/Fermata.Render.js
+++ assets/js/Fermata.Render.js
@@ -4,22 +4,22 @@ var Fermata = Fermata || {};
   "use strict";
 
   Fermata.Render = function (data, container) {
-      this.data = data;
-      this.container = container;
+    this.data = data;
+    this.container = container;
 
-      // Client-side, jQuery selector
-      if (container !== null) {
-        this.container = $(container)[0];
-        this.renderer = new Vex.Flow.Renderer(this.container, Vex.Flow.Renderer.Backends.CANVAS);
-      }
-      // Server-side // TODO
-      else {
-        this.renderer = new Vex.Flow.Renderer(this.container, Vex.Flow.Renderer.Backends.SVG);
-      }
+    // Client-side, jQuery selector
+    if (container !== null) {
+      this.container = $(container)[0];
+      this.renderer = new Vex.Flow.Renderer(this.container, Vex.Flow.Renderer.Backends.CANVAS);
+    }
+    // Server-side // TODO
+    else {
+      this.renderer = new Vex.Flow.Renderer(this.container, Vex.Flow.Renderer.Backends.SVG);
+    }
 
-      this.ctx = this.renderer.getContext();
+    this.ctx = this.renderer.getContext();
 
-      //data.sortMeasure();
-      //console.log(data.getMesure(1, 'P1'));
+  //data.sortMeasure();
+  //console.log(data.getMesure(1, 'P1'));
   };
 }).call(this);
