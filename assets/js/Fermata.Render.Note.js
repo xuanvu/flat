4d449b5f4a1ac1df838a37527e0e7a189ffca9b0
Cyril Coutelier <cyril.coutelier@epitech.eu>
2012-11-08 17:36:37 +0100

    Copie des fonction d'affichage des notes
    Copie des fonctions dans le fichier dédié

--- assets/js/Fermata.Render.Note.js
+++ assets/js/Fermata.Render.Note.js
@@ -3,54 +3,70 @@
  * and open the template in the editor.
  */
 
-  Fermata.Render.prototype.NoteType = 
-  {
-    NORMAL: "normal",
-    CUE: "cue",
-    GRACE: "grace"
-  };
+Fermata.Render.prototype.NoteType = 
+{
+  NORMAL: "normal",
+  CUE: "cue",
+  GRACE: "grace"
+};
 
- Fermata.Render.prototype.renderNote = function(note)
-  {
-    var noteType = this.getNoteType(note);
+Fermata.Render.prototype.renderNote = function(note)
+{
+  var noteType = this.getNoteType(note);
     
-    var processes = [
-    {
-      val: this.NoteType.NORMAL,
-      func: this.renderNormalNote
-    },
-    {
-      val: this.NoteType.CUE,
-      func: this.renderCueNote
-    },
-    {
-      val: this.NoteType.GRACE,
-      func: this.renderGraceNote
-    }];
-  
-    for (var i = 0 ; i < processes.length ; i++)
-    {
-      var process = processes[i];
-      
-      if (process.val === noteType)
-      {
-        process.func(note);
-      }
-    }
-  }
-  
-  Fermata.Render.prototype.getNoteType = function (note)
+  var processes = [
   {
-    if (typeof(note["grace"]) !== "undefined")
+    val: this.NoteType.NORMAL,
+    func: this.renderNormalNote
+  },
+  {
+    val: this.NoteType.CUE,
+    func: this.renderCueNote
+  },
+  {
+    val: this.NoteType.GRACE,
+    func: this.renderGraceNote
+  }];
+  
+  for (var i = 0 ; i < processes.length ; i++)
+  {
+    var process = processes[i];
+      
+    if (process.val === noteType)
     {
-      return this.NoteType.GRACE;
-    }
-    else if (typeof(note["cue"]) !== "undefined")
-    {
-      return this.NoteType.CUE;
-    }
-    else
-    {
-      return this.NoteType.NORMAL;
+      process.func(note);
     }
   }
+}
+  
+Fermata.Render.prototype.getNoteType = function (note)
+{
+  if (typeof(note["grace"]) !== "undefined")
+  {
+    return this.NoteType.GRACE;
+  }
+  else if (typeof(note["cue"]) !== "undefined")
+  {
+    return this.NoteType.CUE;
+  }
+  else
+  {
+    return this.NoteType.NORMAL;
+  }
+}
+  
+Fermata.Render.prototype.renderCueNote = function(cueNote)
+{
+  //TODO: implement
+  }
+  
+Fermata.Render.prototype.renderNormalNote = function(normalNote)
+{
+  //TODO: implement
+  }
+  
+Fermata.Render.prototype.renderGraceNote = function(graceNote)
+{
+  //TODO: implement
+  }
+  
