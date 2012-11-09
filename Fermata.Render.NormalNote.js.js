17569acc3c09c1c3c9f9835f594b0eff8200de2e
Cyril Coutelier <cyril.coutelier@epitech.eu>
2012-11-09 17:59:29 +0100

    creation of a file dedicated to the norma notes

---
+++ Fermata.Render.NormalNote.js.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var Fermata = Fermata || {};
+
+if (typeof(Fermata.Render) === "undefined")
+{
+  throw ("Fermata.Render.js MUST be included before Fermata.Render.NormalNote.js");
+}
+
+(function () {
+  "use strict";
+  
+  
+}).call(this);
