032e9fd0648a31f0e881a29831bf143be60c138e
Vianney Bouchaud <vianney@bouchaud.org>
2012-11-09 03:05:14 +0100

    Grammar correction. 	modified:   Fermata.Render.Harmony.js

--- assets/js/Fermata.Render.Harmony.js
+++ assets/js/Fermata.Render.Harmony.js
@@ -2,67 +2,63 @@ var Fermata = Fermata || {};
 
 if (typeof(Fermata.Render) === "undefined")
 {
-  throw ("Fermata.Render.js MUST be included before Fermata.Render.Harmony.js");
+    throw ("Fermata.Render.js MUST be included before Fermata.Render.Harmony.js");
 }
 
 (function () {
-  "use strict";
+    "use strict";
 
-  Fermata.Render.prototype.HarmonyType =
-  {
-    EXPLICIT: "explicit",
-    IMPLIED: "implied",
-    ALTERNATE: "alternate"
-  };
+    Fermata.Render.prototype.HarmonyType =
+	{
+	    EXPLICIT: "explicit",
+	    IMPLIED: "implied",
+	    ALTERNATE: "alternate"
+	};
 
-  Fermata.Render.prototype.renderHarmony = function (harmony)
-  {
-    var HarmonyType = this.getHarmonyType(harmony);
-
-    var processes = [
+    Fermata.Render.prototype.renderHarmony = function (harmony)
     {
-      val: this.HarmonyType.EXPLICIT,
-      func: function(){
-        this.renderExplicitHarmony();
-      }
-    },
-    {
-      val: this.HarmonyType.IMPLIED
-      func: function(){
-        this.renderImpliedHarmony();
-      }
-    },
-    {
-      val: this.HarmonyType.ALTERNATE,
-      func: function(){
-        this.renderAlternateHarmony();
-      }
-    }];
+	var HarmonyType = this.getHarmonyType(harmony);
 
-
+	var processes = [
+	    {
+		val: this.HarmonyType.EXPLICIT,
+		func: function(){
+		    this.renderExplicitHarmony();
+		}
+	    },
+	    {
+		val: this.HarmonyType.IMPLIED,
+		func: function(){
+		    this.renderImpliedHarmony();
+		}
+	    },
+	    {
+		val: this.HarmonyType.ALTERNATE,
+		func: function(){
+		    this.renderAlternateHarmony();
+		}
+	    }];
 
       //TODO All.
+    }
 
+    Fermata.Render.prototype.getHarmonyType = function (harmony)
+    {
+    }
 
-  }
+    Fermata.Render.prototype.renderCueHarmony = function(explicitHarmony)
+    {
+	//TODO: implement
+    }
 
-  Fermata.Render.prototype.getHarmonyType = function (harmony)
-  {
-  }
+    Fermata.Render.prototype.renderNormalHarmony = function(impliedHarmony)
+    {
+	//TODO: implement
+    }
 
-  Fermata.Render.prototype.renderCueHarmony = function(explicitHarmony)
-  {
-  //TODO: implement
-  }
-
-  Fermata.Render.prototype.renderNormalHarmony = function(impliedHarmony)
-  {
-  //TODO: implement
-  }
-
-  Fermata.Render.prototype.renderGraceHarmony = function(alternateHarmony)
-  {
-  //TODO: implement
-  }
+    Fermata.Render.prototype.renderGraceHarmony = function(alternateHarmony)
+    {
+	//TODO: implement
+    }
 
 }).call(this);
