fca80f5cef8c940bd5f548eca6556d18cd316373
Cyril Coutelier <cyril.coutelier@epitech.eu>
2012-11-09 17:57:22 +0100

    extraction of code into a dedicated file

--- assets/js/Fermata.Render.CueNote.js
+++ assets/js/Fermata.Render.CueNote.js
@@ -14,6 +14,19 @@ if (typeof(Fermata.Render) === "undefined")
 (function () {
   "use strict";
   
-  
+  Fermata.Render.prototype.renderCueNote = function(cueNote)
+  {
+    var obj = this;  
+    var processes = [  
+    {
+      key: "cue",
+      type: this.FuncTypes.DEFAULT,
+      func: null//TODO: implement the function
+    }];
+    this.exploreSubNodes(cueNote, processes);
+      
+    this.renderFullNote(cueNote);
+    var duration = cueNote["duration"]["$t"];
+  }
   
 }).call(this);
--- assets/js/Fermata.Render.Note.js
+++ assets/js/Fermata.Render.Note.js
@@ -72,21 +72,6 @@ if (typeof(Fermata.Render) === "undefined")
     }
   }
   
-  Fermata.Render.prototype.renderCueNote = function(cueNote)
-  {
-    var obj = this;  
-    var processes = [  
-    {
-      key: "cue",
-      type: this.FuncTypes.DEFAULT,
-      func: null//TODO: implement the function
-    }];
-    this.exploreSubNodes(cueNote, processes);
-      
-    this.renderFullNote(cueNote);
-    var duration = cueNote["duration"]["$t"];
-  }
-  
   Fermata.Render.prototype.renderFullNote = function (fullNote)
   {
     var obj = this;  
