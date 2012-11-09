8a025f6a9e74aa1236929abd003544610bb74237
Cyril Coutelier <cyril.coutelier@epitech.eu>
2012-11-09 17:20:47 +0100

    extraction of code in the dedicated file

--- assets/js/Fermata.Render.GraceNote.js
+++ assets/js/Fermata.Render.GraceNote.js
@@ -14,5 +14,41 @@ if (typeof(Fermata.Render) === "undefined")
 (function () {
   "use strict";
   
+   Fermata.Render.prototype.renderGrace = function (grace)
+  {
+    this.renderGraceAttributes(grace);
+  }
+  
+  Fermata.Render.prototype.renderGraceAttributes = function (grace)
+  {
+    var stealTimePrevious = 0;
+    var stealTimeFollowing = 0;
+    var makeTime = 0;
+    var slash = false;
+    
+    if (typeof(grace["steal-time-previous"]) !== "undefined")
+    {
+      stealTimePrevious = grace["steal-time-previous"];
+    }
+    
+    if (typeof(grace["steal-time-following"]) !== "undefined")
+    {
+      stealTimeFollowing = grace["steal-time-following"];
+    }
+    
+    if (typeof(grace["make-time"]) !== "undefined")
+    {
+      makeTime = grace["make-time"];
+    }
+    
+    if (typeof(grace["slash"]) !== "undefined")
+    {
+      if (grace["slash"] === "true")
+      {
+        slash = grace["slash"];
+      }
+    //TODO: what do we do if the value is not false neither true ?
+    }
+  }
   
 }).call(this);
