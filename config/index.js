00b1bcd75c267e470f5e9babb43f40623cc7ad2e
Vincent Giersch <vincent@giersch.fr>
2013-04-29 18:18:24 +0200

    Remove old config file

--- config/index.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-    cookieSecret: '352IbRsD71eur0y4RUj39xmDvnP1QXAnSPT9cySrQCs5iaAfy3D9cSgsX4f7XTi2LoDduDFC03xpgCCNp4V1Lm52JQuDJSPQBEQDCVjXGE0iWmbz3pinzN8YmPbFdpLg',
-    session: {
-      key: 'flat.sid',
-      cookie: { path: '/', httpOnly: false }, 
-      secret: 'qGNhmR73fr6Wv6bmBD6a9v0XSE2QBgPWNQ1FpsogEiXIGMa8hRK9KKzD2dzKa0dsby8DrJs8hA6q7ixAeVbeW4RLgiRfm43LE2GBqGXP2oF1MchFGrx3wEPiGwvJ5FId'
-    }
-};
