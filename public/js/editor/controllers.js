e5c2caac7db519503073a6210e142c75681ae581
gurtne_c <corentin.gurtner@gmail.com>
2013-07-05 17:09:16 +0200

    Change in score.js for completing score informations

--- public/js/editor/controllers.js
+++ public/js/editor/controllers.js
@@ -1,13 +1,18 @@
 'use strict';
 
-function EditorCtrl($scope, $routeParams, Score) {
-  $scope.score = Score.get({id:$routeParams['score']});
-  $scope.data = new Fermata.Data($scope.score);
-  $scope.render = new Fermata.Render($scope.data);
-  $scope.render.renderAll();
-  $scope.drawer = new Fermata.Drawer($scope.data, document.getElementById('canvas-score'));
-  $scope.drawer.drawAll();
-  console.log('ok');
+function EditorCtrl($scope, $routeParams, Score, Revision) {
+  $scope.score = Score.get({id:$routeParams['score']}, function () {
+    $scope.revision = Revision.get({id:$routeParams['score'], revision: $scope.score['revisions'][0].id}, function() {
+      console.log('revision: ', $scope.revision);
+      $scope.data = new Fermata.Data($scope.revision);
+      console.log($scope.data);
+      $scope.render = new Fermata.Render($scope.data);
+      $scope.render.renderAll();
+      $scope.drawer = new Fermata.Drawer($scope.data, document.getElementById('canvas-score'));
+      $scope.drawer.drawAll();
+    });
+  });
+ 
 }
 
-EditorCtrl.$inject = ['$scope', '$routeParams', 'Score'];
+EditorCtrl.$inject = ['$scope', '$routeParams', 'Score', 'Revision'];
