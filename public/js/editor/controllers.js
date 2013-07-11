ebe00280b4497019dd9a6405cb520e36753acd31
Pierre Rannou <pierre@rannou.pl>
2013-07-11 13:25:57 +0200

    add deps raphael and edit basic angular in editor

--- public/js/editor/controllers.js
+++ public/js/editor/controllers.js
@@ -6,13 +6,17 @@ function EditorCtrl($scope, $routeParams, Score, Revision) {
       console.log('revision: ', $scope.revision);
       $scope.data = new Fermata.Data($scope.revision);
       console.log($scope.data);
+      console.log(document.getElementById('canvas-score'));
       $scope.render = new Fermata.Render($scope.data);
       $scope.render.renderAll();
       $scope.drawer = new Fermata.Drawer($scope.data, document.getElementById('canvas-score'));
       $scope.drawer.drawAll();
     });
   });
- 
-}
+
+  $scope.ManageClick = function ($event) {
+    console.log($event);
+  };
+};
 
 EditorCtrl.$inject = ['$scope', '$routeParams', 'Score', 'Revision'];
