95fca89f5381810ea798a366950145ccc53b8fb1
Vincent Giersch <vincent@giersch.fr>
2013-07-15 23:06:14 +0100

    Toolbar first level

--- public/js/editor/flat.editor.toolbarMenu.js
+++ public/js/editor/flat.editor.toolbarMenu.js
@@ -1,28 +1,52 @@
 angular.module('flat.editor.toolbarMenu', []).
 directive('toolbarMenu', function () {
   return {
-    template: '<div class="first"></div><div class="second"></div>',
+    templateUrl: '/views/editor/_toolbarMenu.html',
     controller: ['$rootScope', '$scope', function ($rootScope, $scope) {
-    	$scope.toolbar = {
-    		clef: {
-    			title: 'Clefs',
-    			icon: 'icons-mscore/clef.svg',
-    			subs: [
-    			
-    			]
-    		}
-    		// clef: {
-    		// 	icon: 'icons-mscore/clef.svg',
-    		// 	subs: [
-  				// 	{
-  				// 		icon-m
-    		// 		}
-    		// 	]
-    		// }
-    	};
+      $scope.tools = {
+        clef: {
+          title: 'Clefs',
+          class: 'flat-iconf-treble',
+          subs: [
+            { title: 'Clef Mezzo-soprano', class: 'flat-icons-clef-mezzosoprano' },
+            { title: 'Clef Soprano', class: 'flat-icons-clef-soprano' },
+            { title: 'Clef Mezzo-bass', class: 'flat-icons-clef-bass' }
+          ]
+        },
+        keySignature: {
+          title: 'Keys Signatures',
+          class: 'unicode-icon-sharp',
+          aclass: 'unicode-icon',
+          subs: [
+            { title: 'Clef Mezzo-soprano', class: 'flat-icons-clef-mezzosoprano' },
+            { title: 'Clef Soprano', class: 'flat-icons-clef-soprano' },
+            { title: 'Clef Mezzo-bass', class: 'flat-icons-clef-bass' }
+          ]
+        },
+        note: {
+          title: 'Note',
+          class: 'unicode-icon-eighth',
+          aclass: 'unicode-icon',
+          subs: [
+            { title: 'Clef Mezzo-soprano', class: 'flat-icons-clef-mezzosoprano' },
+            { title: 'Clef Soprano', class: 'flat-icons-clef-soprano' },
+            { title: 'Clef Mezzo-bass', class: 'flat-icons-clef-bass' }
+          ]
+        },
+        player: {
+          title: 'Player',
+          class: 'glyphicon glyphicon-headphones',
+          aclass: 'aglyphicon',
+          subs: [
+            { title: 'Play', class: 'glyphicon glyphicon-play' },
+            { title: 'Stop', class: 'glyphicon glyphicon-stop' }
+          ]
+        }
+      };
 
-
-
+      $scope.showMenu = function (k) {
+        $scope.tools[k].displayed = !$scope.tools[k].displayed;
+      };
       console.log('here');
     }]
   };
