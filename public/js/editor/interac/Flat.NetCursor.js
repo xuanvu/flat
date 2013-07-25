bc688598d7657d8135c42312c38b31c2978ec168
Vincent Giersch <vincent@giersch.fr>
2013-07-25 14:01:50 +0100

    Fixes undefined

--- public/js/editor/interac/Flat.NetCursor.js
+++ public/js/editor/interac/Flat.NetCursor.js
@@ -5,23 +5,22 @@ var Flat = Flat || {};
 
 
   Flat.NetCursor = function(data, ctx, socket, mainUID) {
-  	this.user = [];
-  	this.data = data;
-  	this.context = ctx;
-  	this.socket = socket;
-  	this.mainUID = mainUID;
-  	this.user[mainUID] = new Flat.Cursor(data, ctx, socket, 'black');
-  	this.user[mainUID].initKeyEvents();
+    this.user = [];
+    this.data = data;
+    this.context = ctx;
+    this.socket = socket;
+    this.mainUID = mainUID;
+    this.user[mainUID] = new Flat.Cursor(data, ctx, socket, 'black');
+    this.user[mainUID].initKeyEvents();
   };
 
   Flat.NetCursor.prototype.addGuys = function(uid, color) {
-  	this.user[uid] = new Flat.Cursor(this.data, this.context, this.socket, color);
+    this.user[uid] = new Flat.Cursor(this.data, this.context, this.socket, color);
   };
 
   Flat.NetCursor.prototype.delGuys = function(uid) {
-  	this.user[uid].undraw();
-  	pos = this.user.indexOf(uid);
-  	this.user.splice(pos, 1);
+    this.user[uid].undraw();
+    this.user.splice(this.user.indexOf(uid), 1);
   };
 
   Flat.NetCursor.prototype.updatePosition = function(uid, position) {
@@ -29,7 +28,7 @@ var Flat = Flat || {};
   };
 
   Flat.NetCursor.prototype.getMain = function() {
-  	return this.user[this.mainUID];
+    return this.user[this.mainUID];
   };
 
 }).call(this);
